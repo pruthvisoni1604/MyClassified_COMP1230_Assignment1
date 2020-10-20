0d71c1c3ff20b89b4f46091093568e8fd88c35e0
pruthvisoni1604 <48025357+pruthvisoni1604@users.noreply.github.com>
2020-10-20 18:02:52 -0400

    new

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+ // When the user clicks on the button,toggle between hiding and showing the dropdown content 
+ function dropFunction() {
+     document.getElementById("dropdown").classList.toggle("show");
+ }
+
+ // Closes the dropdown list if the user clicks outside of it
+ window.onclick = function(e) {
+     if (!e.target.matches('.dropBtn')) {
+         var myDropdown = document.getElementById("dropdown");
+         if (myDropdown.classList.contains('show')) {
+             myDropdown.classList.remove('show');
+         }
+     }
+ }
